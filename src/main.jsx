a579fb3d297993c92eb0e4c327538390ed77ee11
Daniel <programador2@intareactiva.net.co>
2025-10-16 12:22:44 -0500

    Update index.html, App.jsx, and styles; add new assets and Precarga page

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './pages/Home'; 
-import Expectativa from './pages/Expectativa';
-import './index.css'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Expectativa from "./pages/Expectativa";
+import Precarga from "./pages/Precarga";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Expectativa />,
-    children: [
-      {
-        path: "/home",
-        element: <Home />,
-      },
-    ],
+  },
+  {
+    path: "/precarga",
+    element: <Precarga />,
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
--- src/pages/Precarga.jsx
+++ src/pages/Precarga.jsx
@@ -2,9 +2,9 @@ import patron from "../assets/patron.png";
 import starAlliance from "../assets/starAlliance.png";
 import logos from "../assets/logos.png";
 
-function Expectativa() {
+function Precarga() {
   return (
-    <div className="page expectativa text-white min-h-scree flex flex-col relative">
+    <div className="page precarga text-white min-h-scree flex flex-col relative">
       <header className="p-5 my-5 flex justify-center md:w-2/5 ">
         <img src={logos} alt="Logos de Avianca y colaboradores" />
       </header>
@@ -12,7 +12,7 @@ function Expectativa() {
       <main className="w-full flex-1 flex flex-col items-center text-start md:text-center justify-center gap-6 px-4">
         <h1>Tu viaje por Latinoamérica empieza aquí</h1>
 
-        <div className=" flex justify-center items-center w-full z-30">
+        <div className=" flex justify-center items-center w-full z-30 ">
           <div
             alt="Ilustración de ventanas de avión cerradas"
             className="ventanasCerradas"
@@ -21,10 +21,11 @@ function Expectativa() {
 
         <div>
           <p className="leading-5 md:leading-8 font-bold text-center z-30 relative">
-            Muy pronto podrás reservar tu lugar <br className="md:hidden" /> en
-            esta experiencia única de avianca, <br />
-            del 20 de octubre al 2 de noviembre <br className="md:hidden" /> en
-            Calle Goya 36, Madrid.
+            En segundos podrás reservar tu experiencia{" "}
+            <br className="md:hidden" /> con avianca,{" "}
+            <br className="hidden md:block" />
+            disponible del 20 de octubre <br className="md:hidden" /> al 2 de
+            noviembre en Calle Goya 36, Madrid.
           </p>
         </div>
       </main>
@@ -51,4 +52,4 @@ function Expectativa() {
   );
 }
 
-export default Expectativa;
+export default Precarga;
